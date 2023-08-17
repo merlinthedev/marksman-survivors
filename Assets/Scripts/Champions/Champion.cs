005561d3ca774c00f83fac20c58c4ce5f3d4cd72
merlijn <merlijnvan11@gmail.com>
2023-08-18 01:26:07 +0200

    public

--- Assets/Scripts/Champions/Champion.cs
+++ Assets/Scripts/Champions/Champion.cs
@@ -93,7 +93,7 @@ public abstract class Champion : AAbilityHolder {
         return m_Rigidbody.velocity.normalized == Vector3.zero ? m_LastKnownDirection : m_Rigidbody.velocity.normalized;
     }
 
-    private float GetGlobalDirectionAngle() {
+    public float GetGlobalDirectionAngle() {
         return m_GlobalMovementDirectionAngle;
     }
 
