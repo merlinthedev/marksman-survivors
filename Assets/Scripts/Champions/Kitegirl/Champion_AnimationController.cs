3396e163208b1a0be81ab5d267c4693c47dd9da3
Fronter <mathijs.klingeman@gmail.com>
2023-08-18 13:52:50 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Champions/Kitegirl/Champion_AnimationController.cs
+++ Assets/Scripts/Champions/Kitegirl/Champion_AnimationController.cs
@@ -1,25 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Champion_AnimationController : MonoBehaviour {
     [SerializeField] private Champion m_ChampionScript;
     [SerializeField] private Animator m_Animator;
+    private bool m_Moving;
 
     private int m_Dir;
 
     private void Update() {
-        m_Dir = (int)Mathf.Floor((m_ChampionScript.GetGlobalDirectionAngle() + 22.5f) / 45.0f) % 8;
-        m_Dir--;
-        if (m_Dir == -1) m_Dir = 7;
-        Debug.Log("GlobalDirectionAngle: " + m_ChampionScript.GetGlobalDirectionAngle() + ", Dir: " + m_Dir, this);
+        m_Dir = (int)Mathf.Floor((m_ChampionScript.GetGlobalDirectionAngle() + 135 + 22.5f) / 45.0f) % 8;
+        Debug.Log(m_Dir);
+        m_Animator.SetInteger("Dir", m_Dir);
+        Move();
     }
 
-    public void Animate() {
+    public void Move() {
+        if (m_ChampionScript.IsMoving && !m_Moving) {
+            m_Animator.SetTrigger("Move");
+            m_Moving = true;
+        }
+        else if (!m_ChampionScript.IsMoving) {
+            m_Moving = false;
+            m_Animator.SetBool("IsMoving", false);
+        }
 
     }
 
     public void Attack() {
-        return;
+        m_Animator.SetTrigger("Attack");
+        m_Animator.SetInteger("Dir", m_Dir);
     }
 }
