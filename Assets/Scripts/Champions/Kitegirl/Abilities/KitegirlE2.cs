1af028922d780cfbd75a379a9e10efd89e122673
merlijn <merlijnvan11@gmail.com>
2023-09-05 17:04:33 +0200

    e2 fix

--- Assets/Scripts/Champions/Kitegirl/Abilities/KitegirlE2.cs
+++ Assets/Scripts/Champions/Kitegirl/Abilities/KitegirlE2.cs
@@ -7,30 +7,14 @@ public class KitegirlE2 : AAbility {
     public override void OnUse() {
         if (IsOnCooldown()) return;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit)) {
-            if (hit.collider.gameObject.CompareTag("Ground")) {
-                Vector3 point = hit.point;
-                point.y = this.m_Champion.transform.position.y - 0.2f;
+        // TODO: Spawn smokescreen
+        this.m_LastUseTime = Time.time;
+        KitegirlSmokescreen kitegirlSmokescreen = Instantiate(m_SmokescreenPrefab,
+            m_Champion.transform.position, Quaternion.identity);
+        kitegirlSmokescreen.OnThrow(m_Champion);
 
-                // Debug.Log("Point: " + point, this);
-
-                if (DistanceCheck(point)) {
-                    // TODO: Spawn smokescreen
-                    this.m_LastUseTime = Time.time;
-                    KitegirlSmokescreen kitegirlSmokescreen = Instantiate(m_SmokescreenPrefab,
-                        m_Champion.transform.position, Quaternion.identity);
-                    kitegirlSmokescreen.OnThrow(m_Champion);
-
-                    m_Champion.Stop();
-                    (m_Champion as Kitegirl)?.SmokeScreenPushBack(m_DashRange);
-                }
-                else {
-                    // Debug.Log("Out of range");
-                }
-            }
-        }
+        m_Champion.Stop();
+        (m_Champion as Kitegirl)?.SmokeScreenPushBack(m_DashRange);
     }
 }
