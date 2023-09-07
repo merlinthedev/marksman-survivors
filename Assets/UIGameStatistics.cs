e660fa1edc5da7284ad6cc4275faf468d4c47788
Fronter <mathijs.klingeman@gmail.com>
2023-09-07 12:50:37 +0200

    Start on timer

---
+++ Assets/UIGameStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameStatistics : MonoBehaviour
+{
+    [SerializeField] private TMP_Text m_Kills;
+    [SerializeField] private TMP_Text m_Gold;
+    [SerializeField] private TMP_Text m_Time;
+
+    private void Update() {
+        //m_Kills.text = "Kills: " + GameManager.Instance.GetKills();
+        //m_Gold.text = "Gold: " + GameManager.Instance.GetGold();
+        //m_Time.text = "Time: " + GameManager.Instance.GetTime();
+    }
+
+}
