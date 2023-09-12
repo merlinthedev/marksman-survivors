2a53e03c1e8e82580f0bb26312fdc4582edf0b2b
merlijn <merlijnvan11@gmail.com>
2023-09-12 14:07:05 +0200

    game timer

---
+++ Assets/Scripts/UI/TimeHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace UI {
+    public class TimeHandler : MonoBehaviour {
+        [SerializeField] private TMP_Text m_TimeText;
+
+        private int seconds;
+        private int minutes;
+
+        private void Start() {
+            StartCoroutine(Clock());
+        }
+
+        private IEnumerator Clock() {
+            while (true) {
+                // TODO: replace later with some type of gamerunning boolean
+                seconds++;
+                if (seconds == 60) {
+                    minutes++;
+                    seconds = 0;
+                }
+
+                m_TimeText.text = $"{minutes:00}:{seconds:00}";
+
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+}
