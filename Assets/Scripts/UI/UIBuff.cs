f978f8b85058b90b4e0ced10aefd25866d166c91
merlijn <merlijnvan11@gmail.com>
2023-09-19 12:38:11 +0200

    move file

---
+++ Assets/Scripts/UI/UIBuff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UI {
+    public class UIBuff : MonoBehaviour {
+        public TMPro.TMP_Text text;
+        public int stacks = 0;
+
+        public void Start() {
+            SetStacks(0);
+        }
+
+        public void SetStacks(int stacks) {
+            this.stacks += stacks;
+            text.text = this.stacks.ToString();
+            if (this.stacks > 0) {
+                this.gameObject.SetActive(true);
+            }
+            else if (this.stacks < 1) {
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        public void SetSprite() { }
+    }
+}
