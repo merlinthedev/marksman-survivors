60f8a14584a5d4863d69a64e2b6c875cae3662a1
merlijn <merlijnvan11@gmail.com>
2023-10-25 13:48:59 +0200

    overdrive start

---
+++ Assets/_Scripts/Champions/Effects/impl/OverdriveEffect.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Champions.Effects.impl {
+    public class OverdriveEffect {
+        [SerializeField] private float percentage = 10f;
+        [SerializeField] private Statistic statistic;
+        
+    }
+}
---
+++ Assets/_Scripts/Champions/Kitegirl/Abilities/Ultimate/Overdrive.cs
@@ -0,0 +1,21 @@
+﻿using _Scripts.BuffsDebuffs.Stacks;
+using _Scripts.Champions.Abilities;
+using _Scripts.Champions.Effects;
+using UnityEngine;
+
+namespace _Scripts.Champions.Kitegirl.Abilities.Ultimate {
+    public class Overdrive : Ability {
+        [SerializeField] private int amountOfStacks = 10;
+        [SerializeField] private Stack.StackType stackType = Stack.StackType.DEFTNESS;
+        [SerializeField] private Effect effect;
+
+        public override void Hook(Champion champion) {
+            base.Hook(champion);
+        }
+
+
+        public override void OnUse() {
+            base.OnUse();
+        }
+    }
+}
