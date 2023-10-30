e851871a47e65e70cadce556b4870457e7921fe6
Fronter <mathijs.klingeman@gmail.com>
2023-10-30 16:40:38 +0100

    fix

--- Assets/_Scripts/Champions/Kitegirl/Abilities/Offense/DevastatingFlare.cs
+++ Assets/_Scripts/Champions/Kitegirl/Abilities/Offense/DevastatingFlare.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 
 namespace _Scripts.Champions.Kitegirl.Abilities.Offense {
     public class DevastatingFlare : Ability, ICastable {
+        [SerializeField] private SerializedDictionary<string, float> enemies = new();
         [SerializeField] private float projectileSpeed;
         [SerializeField] private float damagePercentage = 1f;
+        [SerializeField] private float casttimeFactor = 2f;
         [SerializeField] private Projectile projectilePrefab;
+        private bool piercing = true;
+        private int maxPierce = -1;
         public float CastTime { get; set; }
 
         private Vector3 direction;
@@ -18,8 +22,7 @@ namespace _Scripts.Champions.Kitegirl.Abilities.Offense {
         public override void Hook(Champion champion) {
             base.Hook(champion);
 
-            // CastTime = 2f * champion.GetAttackSpeed();
-            CastTime = champion.GetAttackSpeed();
+            CastTime = casttimeFactor * champion.GetAttackSpeed();
         }
 
         public override void OnUse() {
@@ -28,39 +31,38 @@ namespace _Scripts.Champions.Kitegirl.Abilities.Offense {
             }
 
             Vector3 mousePos = Utilities.GetMouseWorldPosition();
-            direction = (mousePos - this.champion.transform.position).normalized;
+            direction = (mousePos - champion.transform.position).normalized;
 
-            target = this.champion.transform.position + direction * this.abilityRange;
+            target = champion.transform.position + direction * abilityRange;
             target.y = 0.16f;
 
             angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-
 
             Cast();
         }
 
         private void Use() {
-            this.champion.SetIsCasting(false);
+            champion.SetIsCasting(false);
 
 
-            Vector3 pos = new Vector3(this.champion.transform.position.x, 0.16f, this.champion.transform.position.z);
+            Vector3 pos = new Vector3(champion.transform.position.x, 0.16f, champion.transform.position.z);
 
             Projectile projectile = Instantiate(projectilePrefab, pos, Quaternion.Euler(0, angle, 0));
-            projectile.Init(this.champion, target, OnHit, projectileSpeed, this.abilityRange);
+            projectile.Init(champion, target, OnHit, projectileSpeed, abilityRange, piercing);
+
+            base.OnUse();
         }
 
         private void OnHit(IDamageable damageable) {
-            this.champion.DealDamage(damageable, this.champion.GetAttackDamage() * damagePercentage,
+            champion.DealDamage(damageable, champion.GetAttackDamage() * damagePercentage,
                 Champion.DamageType.NON_BASIC);
         }
 
         public void Cast() {
-            base.OnUse();
-
-            (this.champion as Kitegirl)?.GetAnimator().SetDirection(angle);
-            this.champion.SetGlobalDirectionAngle(angle);
-            this.champion.Stop();
-            this.champion.SetIsCasting(true);
+            (champion as Kitegirl)?.GetAnimator().SetDirection(angle);
+            champion.SetGlobalDirectionAngle(angle);
+            champion.Stop();
+            champion.SetIsCasting(true);
 
             Invoke(nameof(Use), CastTime);
         }
--- Assets/_Scripts/Entities/Projectile.cs
+++ Assets/_Scripts/Entities/Projectile.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 namespace _Scripts.Entities {
     public class Projectile : MonoBehaviour {
         [SerializeField] protected float projectileSpeed = 0;
+        public bool piercing;
+        private int pierceCount = 0;
+        public int maxPierce = 1;
         private event Action<IDamageable> OnHit;
         protected IDamager source;
         private Vector3 target;
@@ -15,28 +18,29 @@ namespace _Scripts.Entities {
         protected bool shouldMove = true;
 
         public void Init(IDamager source, Vector3 target, Action<IDamageable> OnHit, float projectileSpeed,
-            float range) {
+            float range, bool piercing = false, int maxPierce = 0) {
             this.source = source;
             this.target = target;
             this.projectileSpeed = projectileSpeed;
             this.OnHit += OnHit;
             this.range = range;
+            this.piercing = piercing;
+            this.maxPierce = maxPierce;
 
             startPoint = source.gameObject.transform.position;
         }
 
-        protected virtual void Update() {
+        private void Update() {
             if (shouldMove) {
                 Move();
             } else {
-                // Debug.Log("Destroying projectile..." + gameObject.name);
+                // Debug.Log("Destroying projectile...");
                 Destroy(gameObject);
             }
         }
 
         protected virtual void Move() {
             transform.position = Vector3.MoveTowards(transform.position, target, projectileSpeed * Time.deltaTime);
-            // Debug.Log("Base Move()");
 
             float dist = Vector3.Distance(startPoint, transform.position);
             // Debug.Log("Distance: " + dist);
@@ -46,15 +50,19 @@ namespace _Scripts.Entities {
                 shouldMove = false;
                 OnHit = null;
             }
+            else if (piercing && pierceCount > maxPierce && maxPierce > 0) {
+                shouldMove = false;
+                OnHit = null;
+            }
         }
 
         protected virtual void OnTriggerEnter(Collider other) {
             IDamageable damageable = DamageableManager.GetInstance().GetDamageable(other);
             if (damageable != null && damageable is not Champion) {
                 OnHit?.Invoke(damageable);
-                shouldMove = false;
-                Destroy(gameObject);
+                if(!piercing) shouldMove = false;
             }
+            if(piercing) pierceCount++;
         }
     }
 }
